0f34a9a36d62090c639a7bd49dabbefbb92f01a1
omarna12 <omarnajjout@gmail.com>
2024-10-09 17:05:08 +0200

    nuevo

---
+++ Universae/Juego.js
@@ -0,0 +1,27 @@
+
+function openModal(src, index) {
+    currentIndex = index;
+    document.getElementById("img01").src = src;
+    document.getElementById("caption").innerHTML = `Imagen ${index + 1} de ${images.length}`;
+    document.getElementById("myModal").style.display = "flex"; 
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none"; 
+}
+
+function changeImage(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) currentIndex = images.length - 1; 
+    if (currentIndex >= images.length) currentIndex = 0; 
+    document.getElementById("img01").src = images[currentIndex];
+    document.getElementById("caption").innerHTML = `Imagen ${currentIndex + 1} de ${images.length}`;
+}
+
+
+window.onclick = function(event) {
+    const modal = document.getElementById("myModal");
+    if (event.target === modal) {
+        closeModal(); 
+    }
+};
